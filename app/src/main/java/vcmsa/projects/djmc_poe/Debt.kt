222dd29205108e138d55de03be824d7a7578c109
Dinaley <131260562+DinaleyMcMurray@users.noreply.github.com>
2025-06-05 11:28:23 +0200

    Adding data classes for debt

---
+++ app/src/main/java/vcmsa/projects/djmc_poe/Debt.kt
@@ -0,0 +1,15 @@
+package vcmsa.projects.djmc_poe
+
+data class Debt(
+    val debtname: String = "",
+    val debtamt: Double = 0.0,
+    val interestamt: Double = 0.0,
+    val npmonthsperiod: Int = 0,
+    val category: String = "",
+    val documentId: String = ""
+)
+{
+    val totalDebtAmount: Double
+        get() = debtamt + interestamt
+
+}
